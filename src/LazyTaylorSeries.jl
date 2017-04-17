005a107db18d07859d2a5763f1577cf20426e4e1
David Sanders <dpsanders@gmail.com>
2017-04-16 21:10:16 -0500

    Add explanatory comments

--- src/LazyTaylorSeries.jl
+++ src/LazyTaylorSeries.jl
@@ -12,17 +12,19 @@ immutable Taylor{T,F,memoize}
     coeffs::Vector{T}
 end
 
-# the function f must take *two* variables
+# the function f must take *two* variables if it is memoized;
 # the first is used as an explicit reference to the current object when necessary
+# In principle this is independent of whether it is memoized
 
 function Taylor{F}(f::F, memoize)
     t = Taylor{Float64,F,Val{memoize}}(f, Float64[])
-    dummy = t[0]  # compile getindex and
+    dummy = t[0]  # compile getindex by calculating first coefficient
     return t
 end
 
-Taylor(f::Function) = Taylor(f, true)
+Taylor(f::Function) = Taylor(f, true)  # memoize by default
 
+# this version of getindex is for non-memoized
 getindex{T,F}(t::Taylor{T,F,Val{false}}, i::Int) = (t.f)(i)
 
 # Memoized; use NaN to indicate value not yet calculated
@@ -32,7 +34,7 @@ function getindex{T,F}(t::Taylor{T,F,Val{true}}, i::Int)
 
     @inbounds if j <= length(coeffs)
         if isnan(coeffs[j])
-            coeffs[j] = (t.f)(t, i)
+            coeffs[j] = (t.f)(t, i)  # pass in the object as the first argument to the function for those functions that are recursive
         end
 
         return coeffs[j]
@@ -47,23 +49,24 @@ function getindex{T,F}(t::Taylor{T,F,Val{true}}, i::Int)
     end
 end
 
+# tt is the independent variable; non-memoized (nothing stored in memory)
 tt = Taylor( i::Int -> (i == 1) * 1.0, false )
 
-
+# use as constant(3); also non-memoized
 constant(c::Float64) = Taylor( i::Int -> (i == 0) ? c : 0.0, false )
 constant(c::Real) = constant(Float64(c))
 
 
+# these are memoized, but should look at performance without memoizing perhaps
 +(f::Taylor, g::Taylor) = Taylor( (t, i) -> f[i] + g[i], true )
 -(f::Taylor, g::Taylor) = Taylor( (t, i) -> f[i] - g[i], true )
 
+
 # formulas from Warwick Tucker, *Validated Numerics*
 
-@inbounds *(f::Taylor, g::Taylor) = Taylor( (t, k) -> sum(f[i] * g[k-i] for i in 0:k), true)
+*(f::Taylor, g::Taylor) = Taylor( (t, k) -> sum(f[i] * g[k-i] for i in 0:k), true)
 
-
-
-# self is a reference to the object exp(g)
+# self is a reference to the object exp(g), that is used recursively
 function exp(g::Taylor)
     function f(self, k)
         k == 0 && return exp(g[0])
