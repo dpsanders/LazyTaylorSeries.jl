5362bb9e065a18b4da1f83d2f1041a0e34febae8
David Sanders <dpsanders@gmail.com>
2019-08-11 12:03:07 -0400

    Add typing to functions

--- src/taylor1.jl
+++ src/taylor1.jl
@@ -43,14 +43,11 @@ import Base: ^
 # the first is used as an explicit reference to the current object when necessary
 # In principle this is independent of whether it is memoized
 
-function Taylor1(f::F, memoize) where F
-    t = Taylor1{Float64,F,Val{memoize}}(f, Float64[])
-    dummy = t[0]  # compile getindex by calculating first coefficient
-    return t
-end
 
-function Taylor1(T, f::F, memoize) where F
-    t = Taylor1{T,F,Val{memoize}}(f, Float64[])
+Taylor1(f::F, memoize) where {F} = Taylor1(Float64, f, memoize)
+
+function Taylor1(T, f::F, memoize) where {F}
+    t = Taylor1{T,F,Val{memoize}}(f, T[])
     dummy = t[0]  # compile getindex by calculating first coefficient
     return t
 end
@@ -83,34 +80,38 @@ function getindex(t::Taylor1{T,F,Val{true}}, i::Int) where {T, F}
 end
 
 # tt is the independent variable; non-memoized (nothing stored in memory)
-tt = Taylor1( i::Int -> (i == 1) * 1.0, false )
+# tt = Taylor1( i::Int -> (i == 1) * 1.0, false )
 
+# tt = Taylor1(Interval{Float64}, (t, i) -> (i == 1) ? (1..1) : (0..0), true)
 # use as constant(3); also non-memoized
-constant(c::Float64) = Taylor1( i::Int -> (i == 0) ? c : 0.0, false )
-constant(c::Real) = constant(Float64(c))
+constant(c::Real) = Taylor1( i::Int -> (i == 0) ? c : 0.0, false )
+constant(T, c::Real) = Taylor1(T, i::Int -> (i == 0) ? c : 0.0, false )
 
 
 # these are memoized, but should look at performance without memoizing perhaps
 
 # use promotion!
 
-+(f::Taylor1, g::Taylor1) = Taylor1( (t, i) -> f[i] + g[i], true )
--(f::Taylor1, g::Taylor1) = Taylor1( (t, i) -> f[i] - g[i], true )
++(f::Taylor1{T}, g::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> f[i] + g[i], true )
+-(f::Taylor1{T}, g::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> f[i] - g[i], true )
 
--(f::Taylor1) = Taylor1( (t, i) -> -f[i], true )
+-(f::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> -f[i], true )
 
--(a::Real, f::Taylor1) = Taylor1( (t, i) -> (i == 0) ? a-f[0] : -f[i], true )
-+(a::Real, f::Taylor1) = Taylor1( (t, i) -> (i == 0) ? a+f[0] : +f[i], true )
+-(a::Real, f::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> (i == 0) ? a-f[0] : -f[i], true )
++(a::Real, f::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> (i == 0) ? a+f[0] : +f[i], true )
 
++(f::Taylor1, a::Real) = a + f
+-(f::Taylor1, a::Real) = f + (-a)
 
 # formulas from Warwick Tucker, *Validated Numerics*
 
-*(f::Taylor1, g::Taylor1) = Taylor1( (t, k) -> sum(f[i] * g[k-i] for i in 0:k), true)
+*(f::Taylor1{T}, g::Taylor1{T}) where {T} = Taylor1(T, (t, k) -> sum(f[i] * g[k-i] for i in 0:k), true)
 
-*(a::Real, f::Taylor1) = Taylor1( (t, i) -> a*f[i], true)
+*(a::Real, f::Taylor1{T}) where {T} = Taylor1(T, (t, i) -> a*f[i], true)
+*(f::Taylor1, a::Real) = a * f
 
 # self is a reference to the object exp(g), that is used recursively
-function exp(g::Taylor1{T,F,memoize}) where {T,F,memoize}
+function exp(g::Taylor1{T}) where {T}
     function f(self, k)
         k == 0 && return exp(g[0])
         # dummy = g[k]  # preallocate g
@@ -133,3 +134,6 @@ function (f::Taylor1)(x)
 
     return total
 end
+
+
+## tt = Taylor1(Interval{Float64}, i -> (i == 1) ? (1..1) : (0..0), false)
