661439e8ca8042f11172b6dc5790594036d336fc
David Sanders <dpsanders@gmail.com>
2019-08-11 12:07:00 -0400

    Move modified power_by_squaring to powers.jl

--- src/taylor1.jl
+++ src/taylor1.jl
@@ -3,41 +3,12 @@ struct Taylor1{T,F,memoize}
     coeffs::Vector{T}
 end
 
-# modified from Base:
-function power_by_squaring(x, p::Integer)
-    if p == 1
-        return copy(x)
-    elseif p == 0
-        return one(x)
-    elseif p == 2
-        return x*x
-    elseif p < 0
-        isone(x) && return copy(x)
-        isone(-x) && return iseven(p) ? one(x) : copy(x)
-        throw_domerr_powbysq(x, p)
-    end
-    t = trailing_zeros(p) + 1
-    p >>= t
-    while (t -= 1) > 0
-        x *= x
-    end
-    y = x
-    while p > 0
-        t = trailing_zeros(p) + 1
-        p >>= t
-        while (t -= 1) >= 0
-            x *= x
-        end
-        y *= x
-    end
-    return y
-end
-
 
 Base.literal_pow(::typeof(^), t::Taylor1, n::Integer) = t^n
 
 import Base: ^
 ^(t::Taylor1, n::Integer) = power_by_squaring(t, n)
+# uses custom version of power_by_squaring in powers.jl file
 
 # the function f must take *two* variables if it is memoized;
 # the first is used as an explicit reference to the current object when necessary
