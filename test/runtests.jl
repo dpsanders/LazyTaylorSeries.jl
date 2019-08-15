20a6d7fe1f537a1e6806f96add16c097b031bffe
David P. Sanders <dpsanders@gmail.com>
2019-08-15 16:59:26 -0400

    Always memoize (#10)
    * Remove unmemoized for simplicity
    
    * Always memoize. Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,29 @@
 using LazyTaylorSeries
-using Base.Test
+using Test
 
-# write your own tests here
-@test 1 == 2
+
+@testset "Basic usage" begin
+    t = Taylor1(Float64, (t, i) -> (i == 1))  # define variable
+    t2 = variable(Float64)
+
+    @test t[0] == 0 == t2[0]
+    @test t[1] == 1 == t2[1]
+    @test t[2] == 0 == t2[2]
+    @test t[100] == 0 == t2[100]
+
+    s = 1 + t
+    @test (s[0], s[1], s[2], s[3]) == (1, 1, 0, 0)
+
+    s = t + t
+    @test (s[0], s[1], s[2], s[3]) == (0, 2, 0, 0)
+
+    s = t * t
+    @test (s[0], s[1], s[2], s[3]) == (0, 0, 1, 0)
+
+    s = t^2
+    @test (s[0], s[1], s[2], s[3]) == (0, 0, 1, 0)
+
+    s = (1 + t)^2
+    @test (s[0], s[1], s[2], s[3]) == (1, 2, 1, 0)
+
+end
